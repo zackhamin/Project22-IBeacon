acce0f214782a182099249b4d10d52c52c3394cd
Zack <ishaqamincoop@icloud.com>
2020-04-27 15:01:58 +0100

    first commit

---
+++ Project22-IBeacons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project22-IBeacons
+//
+//  Created by Ishaq Amin on 27/04/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
